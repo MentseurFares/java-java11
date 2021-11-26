0e737c18b6d77ae0d3111573aa276f2380329845
Mentseur Fares <mentseur.fares@hotmail.fr>
2021-11-26 12:12:57 +0100

    TP 01 - Interface - Default Method

--- src/test/java/java8/ex01/Method_01_Test.java
+++ src/test/java/java8/ex01/Method_01_Test.java
@@ -1,5 +1,7 @@
 package java8.ex01;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.List;
 
 import org.junit.Test;
@@ -19,6 +21,13 @@ public class Method_01_Test {
 
         // TODO créer une méthode int sumAge()
         // TODO Cette méthode retourne le résultat de l'addition des ages des personnes
+        public default int sumAge() {
+        	int totalAge = 0;
+        	for (Person person : findAll()) {
+				totalAge += person.getAge();
+			}
+        	return totalAge;
+        }
     }
     // end::IDao[]
 
@@ -46,11 +55,11 @@ public class Method_01_Test {
     public void test_daoA_sumAge() throws Exception {
 
         DaoA daoA = new DaoA();
-
+        
         // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+        int result = daoA.sumAge();
 
-        assert result == 210;
+        assert  result == 210;
     }
 
     @Test
@@ -59,7 +68,7 @@ public class Method_01_Test {
         DaoB daoB = new DaoB();
 
         // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+        int result = daoB.sumAge();
 
         assert result == 5050;
 
